cb18946c9b62886b9a1091038298804b3aa2da22
Guilherme Staffen <guilhermestaffen@gmail.com>
2025-04-07 08:45:52 -0300

    Adicionando filtro de categoria e a busca por nome

--- restaurante/admin.py
+++ restaurante/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 from .models import Produto, Categoria
 
-admin.site.register(Produto)
-admin.site.register(Categoria)
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'preco', 'categoria')
+    search_fields = ('nome',)
+    list_filter = ('categoria',)
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome_categoria')
+    search_fields = ('nome_categoria',)
